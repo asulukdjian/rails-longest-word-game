e092803abd21264cbcfa4a9addd377f282daddf8
Aram Sulukdjian <asulukdjian@pm.me>
2020-11-09 15:49:42 +0100

    all but optional test done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,20 @@
-class ScoreController < ApplicationController
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
   def new
     @letters = ('a'..'z').to_a.sample(9)
   end
 
   def score
+    word = params[:result]
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    user_word = open(url).read
+    @exist = JSON.parse(user_word)
+    if @exist['found'] == true
+      @count = @exist['length']
+    else
+      @try_again = 'Your word does not exist'
+    end
   end
 end
